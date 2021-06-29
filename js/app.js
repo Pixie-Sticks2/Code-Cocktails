ee19e009f5fbd71d8c1419845a2bb0a11e73eca5
Anvay Bhanap <anvaybhanap@Anvays-MacBook-Pro.local>
2021-06-29 15:49:46 -0700

    added randomDrink function

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 const liquorFormElem = document.getElementById('liquorForm');
 Drink.prototype.allDrinksArray = [];
 let currentDrink = '';
+let drink;
 
 // Constructor Functions
 
@@ -38,6 +39,14 @@ function handleSubmit(event) {
   // render recipe card
 
 }
+
+//user selects Randomize Drink option
+function randomDrink () {
+  let randomNum = Math.floor(Math.random() * Drink.prototype.allDrinksArray.length) + 1;
+  console.log(randomNum);
+  drink = Drink.prototype.allDrinksArray[randomNum];
+}
+
 
 // Event Listener
 liquorFormElem.addEventListener('submit', handleSubmit);
@@ -122,7 +131,8 @@ function createDrinks() {
 
 
 createDrinks();
-
+randomDrink();
+console.log(drink);
 
 // Jesse
 //----------------Prototype Method---------------------//
